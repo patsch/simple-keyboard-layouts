c945c99f6f350fb0f44b791ff062e13a7db6f856
Patrick Dockhorn <patrick@apptastic.com.au>
2024-09-29 05:31:21 +1000

    added germangame.ts as a source file

---
+++ src/lib/layouts/germangame.ts
@@ -0,0 +1,14 @@
+import { LayoutItem } from "../interfaces";
+
+/**
+ * Layout: German
+ */
+export default <LayoutItem>{
+  layout: {
+     default: [
+                "Q W E R T Z U I O P \u00DC",
+                "A S D F G H J K L \u00D6 \u00C4",
+                "{enter} Y X C V B N M \u00C9 {bksp}",
+            ]
+  },
+};
