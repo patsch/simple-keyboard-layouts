92f8e298e847787399bd56be2dd21775467528ac
Patrick Dockhorn <patrick@apptastic.com.au>
2024-09-29 05:09:31 +1000

    added germangame to repo

---
+++ build/layouts/germangame.js
@@ -0,0 +1,12 @@
+/**
+ * Layout: German
+ */
+export default {
+    layout: {
+        default: [
+            "Q W E R T Z U I O P \u00DC",
+            "A S D F G H J K L \u00D6 \u00C4",
+            "{enter} Y X C V B N M \u00C9 {bksp}",
+        ],
+    },
+};
